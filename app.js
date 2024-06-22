f10e433972c8cd95d3348271758a7fef41d88e88
doctafaustus <clowd150@gmail.com>
2024-06-22 17:18:08 -0500

    Add kitty-kush endpoint

--- app.js
+++ app.js
@@ -5,18 +5,16 @@ var Cookies = require('cookies');
 // Globals
 var port = process.env.PORT || 3000;
 
-
 // Initiate Express
 var app = express();
 app.set('view engine', 'ejs');
 
-app.listen(port, function(req, res) {
-	console.log('App listening on port 3000');
+app.listen(port, function (req, res) {
+  console.log('App listening on port 3000');
 });
 
-
 // Routes
-app.get('/', function(req, res) {
+app.get('/', function (req, res) {
   const cookie = new Cookies(req, res);
   const optimizelyClientCookie = cookie.get('optimizelyEndUserId');
   const optimizelyServerCookie = cookie.get('optimizelyEndUserId-server');
@@ -24,7 +22,7 @@ app.get('/', function(req, res) {
   const cookieAttributes = {
     expires: setDate(180),
     domain: 'hmkb.com',
-    sameSite: true
+    sameSite: true,
   };
 
   // First time visitors - no server cookie
@@ -32,36 +30,60 @@ app.get('/', function(req, res) {
     console.log('condition: 1');
     const randomID = generateID();
     cookie.set('optimizelyEndUserId-server', randomID, cookieAttributes);
-    cookie.set('optimizelyEndUserId', randomID,  { ...cookieAttributes, httpOnly: false });
-  } 
-    // Expired ITP visitors - no client cookie but has server cookie
-    else if (!optimizelyClientCookie && optimizelyServerCookie) {
-      console.log('condition: 2');
-    cookie.set('optimizelyEndUserId-server', optimizelyServerCookie, cookieAttributes);
-    cookie.set('optimizelyEndUserId', optimizelyServerCookie,  { ...cookieAttributes, httpOnly: false });
+    cookie.set('optimizelyEndUserId', randomID, {
+      ...cookieAttributes,
+      httpOnly: false,
+    });
+  }
+  // Expired ITP visitors - no client cookie but has server cookie
+  else if (!optimizelyClientCookie && optimizelyServerCookie) {
+    console.log('condition: 2');
+    cookie.set(
+      'optimizelyEndUserId-server',
+      optimizelyServerCookie,
+      cookieAttributes
+    );
+    cookie.set('optimizelyEndUserId', optimizelyServerCookie, {
+      ...cookieAttributes,
+      httpOnly: false,
+    });
   } else {
     console.log('condition: 3');
     // Returning users - refresh existing client and server cookie expirations
-    cookie.set('optimizelyEndUserId-server', optimizelyServerCookie, cookieAttributes);
-    cookie.set('optimizelyEndUserId', optimizelyServerCookie,  { ...cookieAttributes, httpOnly: false });
+    cookie.set(
+      'optimizelyEndUserId-server',
+      optimizelyServerCookie,
+      cookieAttributes
+    );
+    cookie.set('optimizelyEndUserId', optimizelyServerCookie, {
+      ...cookieAttributes,
+      httpOnly: false,
+    });
   }
 
   res.render('index.ejs');
 });
 
-// Redirect to recipe-saver-personal site as a shortcut link 
-app.get('/r', function(req, res) {
+// Redirect to recipe-saver-personal site as a shortcut link
+app.get('/r', function (req, res) {
   res.redirect('https://recipe-saver-personal.herokuapp.com/recipes/all');
+});
+
+app.get('/kitty-kush', function (req, res) {
+  res.redirect('https://kittykush.co?sample=true');
 });
 
 function generateID() {
   const timestamp = new Date().getTime();
-  const randomNum = String(Math.floor(Math.random() * 1000000)).padStart(6, '0');
+  const randomNum = String(Math.floor(Math.random() * 1000000)).padStart(
+    6,
+    '0'
+  );
   return `oeu${timestamp}-${randomNum}.lov`;
 }
 
 function setDate(days = 180) {
-  let targetDate = new Date().getTime() + (days * 24 * 60 * 60 * 1000);
+  let targetDate = new Date().getTime() + days * 24 * 60 * 60 * 1000;
   targetDate = new Date().setTime(targetDate);
   return new Date(targetDate);
 }
